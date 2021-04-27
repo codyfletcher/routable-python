3a6269bd1eb11e39ecdbda030d7dfd36be9fb347
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 10:16:02 -0700

    t (test) - Test instantiation without token parameter

--- routable/tests/test_client.py
+++ routable/tests/test_client.py
@@ -1,3 +1,5 @@
+import pytest
+
 from routable import Client
 
 
@@ -7,3 +9,7 @@ def test_can_instantiate_Client_with_any_string_for_authentication_token():
     client = Client(authentication_token)
 
     assert client is not None
+
+def test_can_not_instantiate_Client_without_authentication_token__raises_TypeError():
+    with pytest.raises(TypeError):
+        _ = Client()

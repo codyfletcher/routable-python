1b2956dcc9cc6018d25a3f4c44d5eaad3042362e
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 10:37:57 -0700

    F client.memberships.list() returns empty list

--- routable/__init__.py
+++ routable/__init__.py
@@ -1,3 +1,16 @@
+class MembershipList:
+    def __init__(self):
+        pass
+
+    def list(self):
+        return []
+
+
 class Client:
     def __init__(self, authentication_token):
         pass
+
+    @property
+    def memberships(self):
+        memberships = MembershipList()
+        return memberships

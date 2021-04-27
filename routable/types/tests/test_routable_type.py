72fdf4f56e6316dca758b42fca0f44b1cc58792c
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 15:07:21 -0700

    t - rename test

--- routable/types/tests/test_routable_type.py
+++ routable/types/tests/test_routable_type.py
@@ -4,7 +4,7 @@ from routable.types.tests.dummy import Dummy
 
 
 class Test_RoutableType:
-    def test__RoutableType__objects_considered_equal__when_class_name_and_ids_match(self):
+    def test__objects_considered_equal__when_class_name_and_ids_match(self):
         object_1 = Dummy.membership()
         object_1.id = "MATCH"
         object_2 = Dummy.membership()

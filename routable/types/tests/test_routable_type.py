c4981b122f5af40b989c637d7b411d6b15007980
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 15:27:15 -0700

    t - Check object not equal when classes are not equal

--- routable/types/tests/test_routable_type.py
+++ routable/types/tests/test_routable_type.py
@@ -1,4 +1,3 @@
-import pytest
 import pytest_check as check
 
 from routable.types.tests.dummy import Dummy
@@ -7,24 +6,25 @@ from routable.types.tests.dummy import Dummy
 class Test_RoutableType:
     def test__objects_considered_equal__when_class_name_and_ids_match(self):
         object_1 = Dummy.membership()
-        object_1.id = "MATCH"
         object_2 = Dummy.membership()
+        object_1.id = "MATCH"
         object_2.id = "MATCH"
 
         assert object_1 == object_2
 
     def test__objects_not_equal__when_ids_do_not_match(self):
         object_1 = Dummy.membership()
-        object_1.id = "Chalk"
         object_2 = Dummy.membership()
+        object_1.id = "Chalk"
         object_2.id = "Cheese"
 
         assert object_1 != object_2
 
-    @pytest.mark.skip("Need a second RoutableType to complete this test")
     def test__objects_not_equal__when_class_names_do_not_match(self):
         object_1 = Dummy.membership()
-        object_2 = Dummy.item()
+        object_2 = Dummy.funding_info_bank()
+        object_1.id = "MATCH"
+        object_2.id = "MATCH"
 
         assert object_1 != object_2
 

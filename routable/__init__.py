bcdd9ec3034a3cea1d784b72969b71151c47e455
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 12:43:54 -0700

    r - Move Client to client.py

--- routable/__init__.py
+++ routable/__init__.py
@@ -1,15 +1,2 @@
+from routable.client import Client
 from routable.list_resource import MembershipList
-
-
-class Client:
-    def __init__(self, authentication_token:str):
-        self.host = "api.sandbox.routable.com"
-        self.headers = {
-            'Authorization': f'Bearer {authentication_token}',
-            'Accept': 'application/vnd.api+json',
-            'Content-Type': 'application/vnd.api+json'
-        }
-
-    @property
-    def memberships(self):
-        return MembershipList(self.host, self.headers)
---
+++ routable/client.py
@@ -0,0 +1,15 @@
+from routable import MembershipList
+
+
+class Client:
+    def __init__(self, authentication_token:str):
+        self.host = "api.sandbox.routable.com"
+        self.headers = {
+            'Authorization': f'Bearer {authentication_token}',
+            'Accept': 'application/vnd.api+json',
+            'Content-Type': 'application/vnd.api+json'
+        }
+
+    @property
+    def memberships(self):
+        return MembershipList(self.host, self.headers)

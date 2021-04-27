4911a1f39b6a065860fa3ea87d8c69182c091087
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 12:24:17 -0700

    r - Extract out host value

--- routable/__init__.py
+++ routable/__init__.py
@@ -2,11 +2,12 @@ import requests
 
 
 class MembershipList:
-    def __init__(self, headers):
+    def __init__(self, host, headers):
+        self.host = host
         self.headers = headers
 
     def list(self):
-        url = "https://api.sandbox.routable.com/memberships/"
+        url = f"https://{self.host}/memberships/"
         response = requests.get(url, headers=self.headers, data={})
         response_dict = response.json()
         return response_dict["data"]
@@ -14,6 +15,7 @@ class MembershipList:
 
 class Client:
     def __init__(self, authentication_token:str):
+        self.host = "api.sandbox.routable.com"
         self.headers = {
             'Authorization': f'Bearer {authentication_token}',
             'Accept': 'application/vnd.api+json',
@@ -22,4 +24,4 @@ class Client:
 
     @property
     def memberships(self):
-        return MembershipList(self.headers)
+        return MembershipList(self.host, self.headers)

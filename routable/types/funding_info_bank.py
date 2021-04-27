a53a2d36079ffcfc267894fc36dae61886bb6a97
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 15:22:55 -0700

    F - Add FundingInfoBank

---
+++ routable/types/funding_info_bank.py
@@ -0,0 +1,6 @@
+from routable.types.routable_type import RoutableType
+
+
+class FundingInfoBank(RoutableType):
+    def __init__(self, data: dict):
+        super().__init__(data)
---
+++ routable/types/tests/test_funding_info_bank.py
@@ -0,0 +1,26 @@
+import pytest
+import pytest_check as check
+
+from routable.types.funding_info_bank import FundingInfoBank
+from routable.types.tests.dummy import Dummy
+
+
+class Test_FundingInfoBank:
+    def test__instantiation__properties_are_set(self):
+        data = {
+            "type": "FundingInfoBank",
+            "id": "abc123"
+        }
+
+        sut = FundingInfoBank(data)
+
+        check.equal("abc123", sut.id)
+
+    @pytest.mark.skip
+    def test__str__(self):
+        funding_info_bank = Dummy.funding_info_bank()
+        funding_info_bank.id = "x"
+
+        sut = str(funding_info_bank)
+
+        check.equal("<FundingInfoBank id=x>", sut)

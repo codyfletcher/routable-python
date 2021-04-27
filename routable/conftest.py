6594db6f26f3265aadb45791712f064d3749bab5
Cody Fletcher <cody@codyfletcher.com>
2021-04-27 14:50:06 -0700

    t - Inline dummy_membership()

--- routable/conftest.py
+++ routable/conftest.py
@@ -1,7 +1,5 @@
 import json
 
-from routable.types.membership import Membership
-
 VALID_STR = "any"
 VALID_BOOL = True
 
@@ -13,19 +11,3 @@ class MockResponse:
 
     def json(self):
         return json.loads(self.response_json_str)
-
-
-def dummy_membership() -> Membership:
-    membership_dict = {
-        "type": "Membership",
-        "id": VALID_STR,
-        "attributes": {
-            "first_name": VALID_STR,
-            "last_name": VALID_STR,
-            "email": VALID_STR,
-            "is_approver": VALID_BOOL,
-            "is_disabled": VALID_BOOL,
-            "avatar": VALID_STR,
-        }
-    }
-    return Membership(membership_dict)
--- routable/types/tests/dummy.py
+++ routable/types/tests/dummy.py
@@ -1,8 +1,20 @@
-from routable.conftest import dummy_membership
+from routable.conftest import VALID_STR, VALID_BOOL
 from routable.types.membership import Membership
 
 
 class Dummy:
     @classmethod
-    def membership(self) -> Membership:
-        return dummy_membership()
+    def membership(cls) -> Membership:
+        data = {
+            "type": "Membership",
+            "id": VALID_STR,
+            "attributes": {
+                "first_name": VALID_STR,
+                "last_name": VALID_STR,
+                "email": VALID_STR,
+                "is_approver": VALID_BOOL,
+                "is_disabled": VALID_BOOL,
+                "avatar": VALID_STR,
+            }
+        }
+        return Membership(data)
